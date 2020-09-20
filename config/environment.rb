bb80e941e8f00e290777cc7aa9c8dcafa3b76d41
Dario Carlino <baires82@gmail.com>
2020-09-20 18:31:16 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,17 +2,12 @@ require "pry"
 require 'bundler'
 Bundler.require
 
-module Concerns
-
-  module Findable
-
+module Concerns:Findable
     def find_by_name(name)
       self.all.detect do |a|
         return a
       end
     end
-
-  end
 
 end
 
