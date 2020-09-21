8e6165ff518ea416ac4f2a2db873435589386559
Dario Carlino <baires82@gmail.com>
2020-09-21 00:41:54 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -6,18 +6,34 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets.chomp
+    input = nil
     while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
       input = gets.chomp
+
+      if input == "list songs"
+        list_songs
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif input == "play song"
+        play_song
+      else
+        "Try something else."
+      end
     end
   end
 
@@ -66,9 +82,13 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    list = Song.all.sort {|a, b| a.name <=> b.name}
-    input = gets.chomp.to_i+1
-    puts "Playing #{list[input].name} by #{list[input].artist.name}"
+    input = gets.strip.to_i
+    if input > 0 && input <= Song.all.length
+      list = Song.all.sort {|a, b| a.name <=> b.name}
+      song = list[input-1]
+      #binding.pry
+      puts "Playing #{song.name} by #{song.artist.name}"
+    end
   end
 
 end
