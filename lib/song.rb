ea2d5e4633999db6a2f519cc0b06c5ade40405ed
Tim Flood <tflood9@gmail.com>
2020-10-03 21:30:36 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,9 @@ class Song
     song
   end
 
-  def self.find_by_name(name)
-    @@all.detect {|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@all.detect {|a| a.name == name}
+  # end
 
   def self.find_or_create_by_name(name)
     song = find_by_name(name)
