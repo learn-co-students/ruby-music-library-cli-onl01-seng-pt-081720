cd1aadf6cb246891084cc945dc137e33e63a13a7
Tim Flood <tflood9@gmail.com>
2020-10-04 15:58:59 -0400

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,4 +1,5 @@
 class MusicLibraryController
+  extend Concerns::Findable
   attr_accessor :path
   attr_reader :song, :artist, :genre
   def initialize(path = "./db/mp3s")
@@ -10,7 +11,6 @@ class MusicLibraryController
     input = ""
     while input != "exit"
       puts "Welcome to your music library!"
-      puts "What would you like to do?"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
@@ -18,23 +18,29 @@ class MusicLibraryController
       puts "To list all of the songs of a particular genre, enter 'list genre'."
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
+      puts "What would you like to do?"
         input = gets.strip
-      case input
-      when "list songs"
-        songs
-      when "list artists"
-        artists
-      when "list genres"
-        genres
-      when "list artists"
-        list_artist
-      when "list genres"
-        list_genres
-      when "play song"
-        play_song
+        case input
+        when 'list songs'
+          self.list_songs
+        when 'list artists'
+          self.list_artists
+        when 'list genres'
+          self.list_genres
+        when 'list artists'
+          self.list_artists
+        when 'list artist'
+          self.list_songs_by_artist
+        when 'list genre'
+          self.list_songs_by_genre
+        when 'play song'
+          self.play_song
+        else
+          "Type in a valid request please"
+        end
       end
+
     end
-  end
 
   def list_songs
     songs = Song.all
@@ -43,8 +49,56 @@ class MusicLibraryController
   end
 
   def list_artists
-    songs = Song.all
-    alph_list = songs.sort_by!{ |artist| song.artist.name }
-    alph_list.each_with_index { |song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
+    artists = Artist.all
+    alph_list = artists.sort_by! { |artist| artist.name }
+    alph_list.each_with_index { |artist, index| puts "#{index+1}. #{artist.name}" }
   end
+
+  def list_genres
+    genres = Genre.all
+    alph_list = genres.sort_by! { |genre| genre.name }
+    alph_list.each_with_index { |genre, index| puts "#{index+1}. #{genre.name}" }
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
+        puts "#{i}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
+        puts "#{i}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.chomp.to_i
+
+    # self.list_songs
+    songs = Song.all
+    # songs.sort {|a,b| a.name <=> b.name}
+
+
+
+    if (1..songs.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
+    # binding.pry
+
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+
+
 end
